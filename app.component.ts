49e562a408af1cdc13f805ecc224938c430851eb
miccou <30512204+miccou@users.noreply.github.com>
2024-03-13 17:51:15 +1100

    complex scroll solution

--- app.component.ts
+++ app.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+  viewChild,
+} from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -8,5 +14,32 @@ import { Component } from "@angular/core";
   templateUrl: "app.component.html",
 })
 export class AppComponent {
+  @ViewChild("confirmationPanel") confirmationPanel!: ElementRef;
+  @ViewChild("keySummaryPanel") keySummaryPanel!: ElementRef;
+
   title = "angular-tailwind-minimal";
+
+  shouldFixConfirmationPanel = false;
+
+  @HostListener("scroll", ["$event"])
+  onElementScroll($event: Event) {
+    const confirmationPanelElement = this.confirmationPanel
+      .nativeElement as HTMLDivElement;
+    const keySummaryElement = this.keySummaryPanel
+      .nativeElement as HTMLDivElement;
+
+    console.log(
+      "top of confirmation panel",
+      keySummaryElement.getBoundingClientRect().top
+    );
+    if (keySummaryElement.getBoundingClientRect().top - 50 - 32 < 0) {
+      console.log("should fix confirmation panel");
+
+      this.shouldFixConfirmationPanel = true;
+    } else {
+      console.log("should unfix confirmation panel");
+
+      this.shouldFixConfirmationPanel = false;
+    }
+  }
 }
